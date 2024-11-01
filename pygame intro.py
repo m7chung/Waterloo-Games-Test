fa79a6ff7638c69407a47c15c978b172e69b312f
Madeline Chung <m7chung@uwaterloo.ca>
2024-10-31 20:48:25 -0400

    Update pygame intro.py

--- pygame intro.py
+++ pygame intro.py
@@ -4,7 +4,6 @@ Created on Wed Oct 30 17:45:29 2024
 
 @author: msmad
 """
-
 import pygame
 #need to import so exit is supported --> doesn't run error
 from sys import exit
@@ -28,16 +27,17 @@ ANANA_surf = pygame.image.load('ANANA.png').convert_alpha()
 #test_surf.fill('cadetblue3')
 cursedcat_surf = pygame.image.load('cursed cat.JPG').convert_alpha()
 # render(text_surf, anti-aliase, color)
-score_surf = test_font.render('Baddies', False, 'chartreuse1')
+score_surf = test_font.render('Baddies', False, (0,255,0))
 score_rect = score_surf.get_rect(center = (500, 50))
 
 bibble_surf = pygame.image.load('bibble.png').convert_alpha()
-bibble_rect = bibble_surf.get_rect(bottomright = (700,400))
+bibble_rect = bibble_surf.get_rect(bottomright = (2000,750))
 
 #go to the-image-editor.com/image/resize to change the dimension of a image if too large
 tikka_surf = pygame.image.load('tikka.JPG').convert_alpha()
 #Rect(left,top,width,height) --> rectangle in same place as tikka
 tikka_rect = tikka_surf.get_rect(bottomright = (200,400))
+tikka_gravity = 0
 
 #bibble_rect = bibble_surf.get_rect(midtop = (bibble_x_pos,500))
 
@@ -50,28 +50,61 @@ while True:
             pygame.quit()
             exit()
             
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE and tikka_rect.bottom >= 750:
+                tikka_gravity = -15
+                
+
+                
 #MOUSE MOTION or MOUSEUP / MOUSEDOWN to detect when mouse button clicked(down)
-       # if event.type == pygame.MOUSEMOTION:
-        #    tikka_rect.collidepoint(event.pos): 
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if tikka_rect.collidepoint(event.pos) and tikka_rect.bottom >= 750:
+                tikka_gravity =-5
 #blit = block image transfer --> put on surface on another surface --> (200,100) means we want our display surface 200 from left 100 from top
  # Fill the screen with a background color or image
     screen.fill((0, 0, 0))  # Fill the screen with black color, alternatively you can blit a background image
     
     screen.blit(ANANA_surf,(0,0))
     screen.blit(cursedcat_surf,(600,200))
+    pygame.draw.rect(screen,"Pink", score_rect,10)
+    pygame.draw.rect(screen,"Pink", score_rect,)
     screen.blit(score_surf, score_rect)
     #bibble will move 60 pixels per second
    
-    bibble_rect.x -= 4
+    pygame.draw.line(screen,"Silver",(0,0),pygame.mouse.get_pos(),10)
+    pygame.draw.ellipse(screen,'Turquoise',pygame.Rect(50,400,100,100))
+    
+    bibble_rect.x -= 12  
     if bibble_rect.right <= 0:
         bibble_rect.left = 1000
     
     screen.blit(bibble_surf,(bibble_rect))
     
-    tikka_rect.left
-   
+    tikka_gravity += 0.4
+    tikka_rect.y += tikka_gravity
+    if tikka_rect.bottom >= 750:
+        tikka_rect.bottom = 750
+        
+    if tikka_rect.top <= 50:
+        tikka_rect.top = 50
+    screen.blit(tikka_surf,tikka_rect)
     
-    screen.blit(tikka_surf, tikka_rect)
+    
+#    keys = pygame.key.get_pressed() 
+#    if keys[pygame.K_SPACE]:
+#        print('jump')
+
+
+
+#COLLISION --------------------------------------------------
+
+    if bibble_rect.colliderect(tikka_rect):
+        pygame.quit()
+        exit()
+    
+    
+
+
 # draw all our elements
 # update everything
     pygame.display.update()
@@ -80,6 +113,6 @@ while True:
     
 #if tikka_rect.colliderect(bibble_rect):
     
-mouse_pos = pygame.mouse.get_pos()
-if tikka_rect.collidepoint(mouse_pos):
-    pygame.mouse.get_pressed()
+#mouse_pos = pygame.mouse.get_pos()
+#if tikka_rect.collidepoint(mouse_pos):
+#    pygame.mouse.get_pressed()
